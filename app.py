4fe6f4e9cbf41e3c33a3a0c2cf7fa1694c276093
VenzoG <32364218+VenzoG@users.noreply.github.com>
2022-05-23 22:41:37 +1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+from datetime import timedelta
+
+from seats import have_seats , change_datetime, model
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    switch_str = {"shift":request.form['shift'],
+                  "date":request.form['date'],
+                  "start_time":request.form['start_time'],
+                  "end_time":request.form['end_time'],
+                  "confirmation":request.form['confirmation'],
+                  "cut":request.form['cut'] }
+    switch = change_datetime.change(switch_str)
+    # time_until string
+    time_until_str = change_datetime.time_until(switch)
+    
+    # shift string
+    if (switch['shift']=="gck"):
+        shift_str = "You have a GC kitchen shift."
+    elif (switch['shift']=="gcb"):
+        shift_str = "You have a GC bar sales shift."
+    elif (switch['shift']=="gcr"):
+        shift_str = "You have a GC runner shift."
+    elif (switch['shift']=="gch"):
+        shift_str = "You have a GC host shift."
+    elif (switch['shift']=="gcs"):
+        shift_str = "You are a GC supervisor."
+    
+    #result string
+    result_str = ""
+    if (switch['shift'] == 'gcs'):
+        result_str = result_str + "As a supervisor, it is unlikely your shift will be cut. "
+    elif (switch['confirmation'] == 'no'):
+        result_str = result_str + "As your shift has not yet been confirmed, it is likely your shift will be cut further. "
+    elif (switch['cut'] == 'yes'):
+        result_str = result_str + "As your shift has already been cut, it is unlikely your shift will be cut further. "
+    else:
+        result_str = "Be wary! "
+        
+    result_str = result_str + "Here is the predictive percentage that your shift will be cut further:"
+    
+    df, function = have_seats.grab(switch['date'])
+    #remove all non-shift sessions of day
+    df=df[(switch['start_time'] - timedelta(minutes=20)) < df['end_time']]
+    df=df[switch['end_time'] > df['end_time']]
+    
+    movies_str = ["[Past or non-public session]"]*10
+    for i, rows in df.iterrows():
+        start_hr = str(rows['session_time'].hour)
+        if (rows['session_time'].minute >= 10):
+            start_min = str(rows['session_time'].minute)
+        else:
+            start_min = "0" + str(rows['session_time'].minute)
+            
+        end_hr = str(rows['end_time'].hour)
+        if (rows['end_time'].minute >= 10):
+            end_min = str(rows['end_time'].minute) 
+        else:
+            end_min = "0" + str(rows['end_time'].minute) 
+        
+        movies_str[i] = rows["movie"] + " " + start_hr + ":" + start_min + " - " + end_hr + ":" + end_min + ": "
+        movies_str[i] = movies_str[i] + str(100*round(rows['taken_percent'],2))+ "%."
+
+    # seats full string
+    s_pc = 100*(sum(df['taken_percent'])/len(df['taken_percent']))
+    seats_full_str = "Currently, there are " + str(round(s_pc,2)) + "% of seats booked in the sessions on during your shifts:"
+    # percentage string
+    percent_str = model.make_model(switch, df, function)
+
+    return render_template('prediction.html', 
+                           shift_type=(shift_str),
+                           time_til=(time_until_str),
+                           seats_full=(seats_full_str),
+                           result=(result_str),
+                           movies_0=(movies_str[0]),
+                           movies_1=(movies_str[1]),
+                           movies_2=(movies_str[2]),
+                           movies_3=(movies_str[3]),
+                           movies_4=(movies_str[4]),
+                           movies_5=(movies_str[5]),
+                           movies_6=(movies_str[6]),
+                           movies_7=(movies_str[7]),
+                           movies_8=(movies_str[8]),
+                           movies_9=(movies_str[9]),
+                           percent=(percent_str))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ seats.py
@@ -0,0 +1,251 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from datetime import datetime, timedelta
+import pandas as pd
+
+def get_seats(date):
+    t_year = str(date.year)
+
+    if date.month > 10:
+        t_month = str(date.month)
+    else:
+        t_month = "0" + str(date.month)
+
+    t_day = str(date.day)
+    
+    s=Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
+    driver.implicitly_wait(10);
+    driver.get("https://www.eventcinemas.com.au/Sessions#cinemas=64&exp=GC&date=" + t_year + "-" + t_month+ "-"+ t_day);
+    
+    sess_list = []
+    movies = driver.find_elements(By.XPATH, "//*[@class='movie-list-item movie-container-item']")
+
+    if (len(movies) == 0):
+        return "unconfirmed", False
+    
+    for i in range(len(movies)):
+        movies_list = driver.find_elements(By.XPATH, "//*[@class='movie-list-item movie-container-item']")
+        m = movies_list[i]
+        m_name = m.get_attribute('data-name')
+        print("MOVIE NAME: ", m_name)
+
+
+        #get runtime of movie
+        print("//*[@data-name='"+m_name+"']/div[2]/div/a")
+        driver.find_element(By.XPATH, '//*[@data-name="'+m_name+'"]/div[2]/div/a').click()
+        print("CURRENT RUNTIME INVESTIIGATION FOR THIS MOVIE: ", driver.current_url)
+        desktop_elements = driver.find_elements(By.XPATH, "//*[@class='prop ']/span")
+        text_elements = driver.find_elements(By.XPATH, "//*[@class='prop ']/p")
+        runtime = -1
+        cinema=-1
+        if (len(desktop_elements) > 0):
+            for i, element in enumerate(desktop_elements):
+                print(element.text)
+                print(text_elements[i].text)
+                if (element.text == "Running Time:"):
+                    runtime = int(text_elements[i].text[0:3])
+                    cinema=0
+                    break
+        driver.back()
+
+
+        # once movie is isolated, determine which data-screentype=GC elements are present
+        sessions = driver.find_elements(By.XPATH, 
+        '//*[@data-name="'+m_name+'"]/div[2]/div[2]/div[1]/div/*[@data-screentype="GC"]')
+
+        #details for session
+        for sess in sessions:
+            sess_yr = int(sess.get_attribute("data-time")[0:4])
+            sess_mth = int(sess.get_attribute("data-time")[5:7])
+            sess_day = int(sess.get_attribute("data-time")[8:10])
+            sess_hr = int(sess.get_attribute("data-time")[11:13])
+            sess_min = int(sess.get_attribute("data-time")[14:17])
+            sess_time = datetime(sess_yr, sess_mth, sess_day, sess_hr, sess_min)
+            end_time = sess_time + timedelta(minutes=runtime)
+
+            seats = int(sess.get_attribute("data-seatsavailable"))
+
+            sess_row = {"session_time":sess_time, "end_time":end_time, "cinema":cinema, "movie":m_name, "runtime":runtime, "seats_available":seats}
+            sess_list.append(sess_row)
+            print("*SESSION DETAILS*", sess_row)
+
+    df = pd.DataFrame(sess_list)
+    df.sort_values("session_time", inplace=True)
+    df.reset_index(inplace=True)
+    df.drop(["index"], axis=1, inplace=True)
+    df.loc[0, "cinema"] = 1
+    df['taken_percent'] = 1-df['seats_available']/40
+    curr_sess=0
+    
+
+    
+    while (df['cinema'].min() == 0):
+        # start with cinema 1 stream
+        if (df['cinema'].value_counts()[1] == 1):
+            print("CURR CINEMA = 1")
+            curr_cinema=1 
+        # else, understand the cinema 2 stream
+        else:
+            print("CURR CINEMA = 2")
+            curr_cinema=2
+            # find un assigned cinema row
+            for row in df.iterrows():
+                if (row[1]['cinema']==1):
+                    continue
+                else:
+                    print("CURR_SESS = ", row[0])
+                    curr_sess = row[0]
+                    df.loc[row[0], 'cinema'] = curr_cinema
+                    break
+
+        while (curr_sess != -1):
+            # determine cleaning period and likelihood of next session
+            print("DETERMINING NEW SET")
+            new_assign=0
+            curr_end = df.loc[curr_sess, "end_time"]
+            curr_end_min  = curr_end + timedelta(minutes = 19)
+            curr_end_max =  curr_end + timedelta(minutes = 45)
+            # check in rows where the cinema is not equal to 0
+            for row in df[df['cinema']==0].iterrows():
+                print("CHECKING ROW")
+                next_end = row[1]['session_time']
+                print(curr_end_min, curr_end_max)
+                print(next_end)
+                if (curr_end_min < next_end and curr_end_max > next_end):
+                    print("*CINEMA CHANGE*")
+                    df.loc[row[0], 'cinema'] = curr_cinema
+                    curr_sess = row[0]
+                    new_assign=1
+                    break
+
+            # if we cannot find a row to assign a new session, break
+            if (new_assign==0):
+                curr_sess=-1
+                print("CANNOT FIND NEW SET")
+
+        curr_sess = 0 
+        while (curr_sess != -1):
+        # determine cleaning period and likelihood of next session
+            print("DETERMINING NEW SET WITH EXPANDED PARAMETERS FOR CLEANUP OF SPARES")
+            new_assign=0
+            # these are the changed lines of code
+            curr_end = df[df['cinema']==0].iloc[curr_sess]["end_time"]
+            curr_cinema = df.loc[(df[df['cinema']==0].index[0]-1), 'cinema']
+            if (curr_cinema == 1):
+                curr_cinema=2
+            elif (curr_cinema == 2):
+                curr_cinema=1
+            print("****Cinema*******", curr_cinema)
+
+            curr_end_min  = curr_end + timedelta(minutes = 20)
+            curr_end_max =  curr_end + timedelta(minutes = 70)
+            # check in rows where the cinema is not equal to 0 for proceeding session
+            for row in df.iterrows():
+                print("CHECKING ROW")
+                next_end = row[1]['session_time']
+                print(curr_end_min, curr_end_max)
+                print(next_end)
+                if (curr_end_min < next_end and curr_end_max > next_end):
+                    print("*CINEMA CHANGE OF PREVIOUSLY BLANK ROW*")
+                    df.loc[df[df['cinema']==0].index[0], 'cinema'] = curr_cinema
+                    if (len(df[df['cinema']==0]) != 0):
+                        new_assign=1
+                    break
+
+        # determine cleaning period and likelihood of previous session
+            print("DETERMINING NEW SET WITH EXPANDED PARAMETERS FOR CLEANUP OF SPARES - PREVIOUS SESSION MATCH")
+            new_assign=0
+            # these are the changed lines of code
+            curr_end = df[df['cinema']==0].iloc[curr_sess]["session_time"]
+            curr_cinema = df.loc[(df[df['cinema']==0].index[0]-1), 'cinema']
+            if (curr_cinema == 1):
+                curr_cinema=2
+            else:
+                curr_cinema=1
+            print("****Cinema*******", curr_cinema)
+
+            curr_end_min  = curr_end + timedelta(minutes = 20)
+            curr_end_max =  curr_end + timedelta(minutes = 70)
+
+            # check in rows where cinema is assigned for preceeding session
+            for row in df.iterrows():
+                print("CHECKING ROW")
+                next_end = row[1]['end_time']
+                print(curr_end_min, curr_end_max)
+                print(next_end)
+                if (curr_end_min < next_end and curr_end_max > next_end):
+                    print("*CINEMA CHANGE OF PREVIOUSLY BLANK ROW*")
+                    df.loc[df[df['cinema']==0].index[0], 'cinema'] = curr_cinema
+                    if (len(df[df['cinema']==0]) != 0):
+                        new_assign=1
+                    break
+
+            # if we cannot find a row to assign a new session, break
+            if (new_assign==0):
+                curr_sess=-1
+                print("CANNOT FIND NEW SET")
+    
+    #is there a function on
+    prev_cinema = 2
+    function = False
+    for row in df.iterrows():
+        if (prev_cinema != row[1]['cinema']):
+            prev_cinema = row[1]['cinema']
+        else:
+            print("DISCREPENCY DETECTED")
+            print("There may be a private session, two consecutive sessions are located in the same cinema.")
+            function = True
+    
+    return df, function
+
+class have_seats():
+    def __init__(self):
+        pass
+    
+    def grab(switch):
+        df, function = get_seats(switch)
+        return df, function
+
+class change_datetime():
+    def __init__(self):
+        pass
+    
+    def change(switch_str):
+        date_str = switch_str['date']
+        switch_str['date'] = datetime(year=int(date_str[0:4]),
+                                            month=int(date_str[5:7]),
+                                            day=int(date_str[8:10]))
+        
+        start_str = switch_str['start_time']
+        switch_str['start_time'] = datetime(year=int(date_str[0:4]),
+                                            month=int(date_str[5:7]),
+                                            day=int(date_str[8:10]),
+                                            hour=int(start_str[0:2]), 
+                                            minute=int(start_str[3:5]))
+        end_str = switch_str['end_time']
+        switch_str['end_time'] = datetime(year=int(date_str[0:4]),
+                                            month=int(date_str[5:7]),
+                                            day=int(date_str[8:10]),
+                                            hour=int(end_str[0:2]), 
+                                            minute=int(end_str[3:5]))
+        return switch_str
+    
+    def time_until(switch):
+        time_til_shift = (switch['start_time'] - datetime.now())
+        time_until_str = "There are still " + str(time_til_shift.days) + " days and " + str(round(time_til_shift.seconds/60/60))  + " hours until your shift. "
+        return time_until_str
+    
+class model():
+    def __init__(self):
+        pass
+    
+    def make_model(switch, df, function):
+
+        return "[Placeholder! Percentage predictive model isn't done yet.]"
