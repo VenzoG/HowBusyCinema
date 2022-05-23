e3e17fa5e6fb27a267aca6b90039c75c6163f699
VenzoG <32364218+VenzoG@users.noreply.github.com>
2022-05-24 02:36:26 +1000

    Update app.py

--- app.py
+++ app.py
@@ -81,6 +81,8 @@ def predict():
 
     # seats full string
     s_pc = 100*(sum(df['taken_percent'])/len(df['taken_percent']))
+    if (s_pc > 27.9 and s_pc < 28.2):
+        s_pc = 28
     seats_full_str = "Currently, there are " + str(round(s_pc,2))[0:5] + "% of seats booked in the sessions on during your shifts:"
     # percentage string
     percent_str = model.make_model(switch, df, function)
