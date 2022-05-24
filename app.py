0d3d33dbaea9268837238a95dfa555f1be8e711d
VenzoG <32364218+VenzoG@users.noreply.github.com>
2022-05-24 15:00:19 +1000

    Update app.py

--- app.py
+++ app.py
@@ -60,12 +60,12 @@ def predict():
     print(type(switch))
     print(switch)
     # if shift occurs at least 20 min after end of start of shift
-    df=df[(switch['start_time'] - timedelta(minutes=20)) < df['end_time']]
+    df=df[(switch['start_time'] - timedelta(minutes=20)) =< df['end_time']]
     print(df)
     print("2ND")
     df0 = df.copy()
     # if shift occurs at least 20 min
-    df=df[switch['end_time'] > df['session_time']]
+    df=df[switch['end_time'] >= df['session_time']]
     print(df)
     print("3RD")
     
